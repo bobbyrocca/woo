75cf5fda5d48f4f60350332246d72c2574195aab
bobbyrocca <100429929+bobbyrocca@users.noreply.github.com>
2023-12-14 19:14:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# The Woo Project
---
+++ wp-content/plugins/adrocket-404/adrocket.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Plugin Name: Adrocket Custom 404 Redirect
+ * Description: Redirects specified pages to a 404 page.
+ * Version: 1.0
+ * Author: Halexo Limited
+ */
+
+add_action( 'template_redirect', 'custom_404_redirect' );
+
+function custom_404_redirect() {
+	$redirect_pages = [
+		'blog',
+		'another-page',
+		'sample-page',
+		'archive',
+		'category',
+		'tag',
+		'author',
+		'year',
+		'month',
+		'day',
+	]; // Aggiungi gli slug delle pagine qui
+
+	if ( in_array( $GLOBALS['wp_query']->query_vars['pagename'], $redirect_pages ) ) {
+		global $wp_query;
+		$wp_query->set_404();
+		status_header( 404 );
+		get_template_part( 404 );
+		exit();
+	}
+
+	// also disable all nested pages e.g. year/month/day
+
+	if ( is_year() || is_month() || is_day() ) {
+		global $wp_query;
+		$wp_query->set_404();
+		status_header( 404 );
+		get_template_part( 404 );
+		exit();
+	}
+
+	// also disable all nested categories directories
+
+	if ( is_category() || is_tag() || is_author() ) {
+		global $wp_query;
+		$wp_query->set_404();
+		status_header( 404 );
+		get_template_part( 404 );
+		exit();
+	}
+
+	// also disable all nested non shop pages
+
+	if ( is_page() && ! is_shop() && ! is_cart() && ! is_checkout() && ! is_account_page() ) {
+		global $wp_query;
+		$wp_query->set_404();
+		status_header( 404 );
+		get_template_part( 404 );
+		exit();
+	}
+}
