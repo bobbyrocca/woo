e04afd89ca82cce94564a5c419d73404fb227b81
bobbyrocca <100429929+bobbyrocca@users.noreply.github.com>
2024-01-15 09:53:38 +0000

    Quantity selector v1.1

---
+++ wp-content/themes/adrocket/patterns/hidden-search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Search
+ * Slug: adrocket/hidden-search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'adrocket' ); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'adrocket' ); ?>","fontSize":"medium"} /-->
