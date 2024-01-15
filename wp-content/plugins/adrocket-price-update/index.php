17c5d955b42da1ab291c2087fbeab9606d73273d
bobbyrocca <100429929+bobbyrocca@users.noreply.github.com>
2024-01-15 10:16:47 +0000

    Quantity selector v1.2

--- wp-content/plugins/adrocket-price-update/index.php
+++ wp-content/plugins/adrocket-price-update/index.php
@@ -14,6 +14,10 @@ function get_updated_price_callback() {
 	$total_regular_price = 0;
 	$total_sale_price    = 0;
 
+	$price_for_one   = 0;
+	$price_for_two   = 0;
+	$price_for_three = 0;
+
 	if ( empty( $variation_ids ) ) {
 		$product = wc_get_product( $product_id );
 
@@ -52,13 +56,34 @@ function get_updated_price_callback() {
 				continue; // Salta se il prodotto non esiste
 			}
 
-			$regular_price = floatval( $product->get_regular_price() );
-			$sale_price    = floatval( $product->get_sale_price() ) ?: $regular_price;
+			$id_to_use = $variation_id ?: $product_id;
+
+			// Prezzi basati sulla quantità per la variante
+			$price_for_one   = get_post_meta( $id_to_use, 'qty_based_price_1', true );
+			$price_for_two   = get_post_meta( $id_to_use, 'qty_based_price_2', true );
+			$price_for_three = get_post_meta( $id_to_use, 'qty_based_price_3', true );
+
+			// Calcola il prezzo in base alla quantità
+
+			if ( $quantity == 1 && ! empty( $price_for_one ) ) {
+				$sale_price = $price_for_one;
+			} elseif ( $quantity == 2 && ! empty( $price_for_two ) ) {
+				$sale_price = $price_for_two;
+			} elseif ( $quantity >= 3 && ! empty( $price_for_three ) ) {
+				$sale_price = $price_for_three;
+			} else {
+				$sale_price = $product->get_sale_price() ?: $product->get_regular_price();
+			}
+
+			$regular_price = $product->get_regular_price();
 
 			// Calcola il prezzo per ogni variante (considerando una quantità di 1 per variante)
 			$total_regular_price += $regular_price;
-			$total_sale_price    += $sale_price;
+			$total_sale_price    += $sale_price / $quantity;
 		}
+
+		$total_regular_price = floatval( $total_regular_price );
+		$total_sale_price    = floatval( $total_sale_price );
 	}
 
 	// Costruisci e invia la risposta JSON
